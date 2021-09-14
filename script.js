5fe18452559442a63d7639417a2fe957301f4d9f
Daiane Silva <daianemsilva09@gmail.com>
2021-09-14 16:48:17 -0300

    inserir dados

--- script.js
+++ script.js
@@ -3,8 +3,23 @@ function login() {
   var senha = document.getElementById('txtsenha')
 
   if (email.value.length == 1 && senha.value.length == 1 ){
-    window.location.href = "http://pt.stackoverflow.com";
+    window.location.href = "https://daianemsilva09.github.io/finanday/principal";
   }else{
     window.alert('Login ou senha incorreto')
   }
+  }
+
+  function enviar(){
+    var valor = document.getElementById('valor')
+    var roud1 = document.getElementsByName('novins')
+    
+      if (valor.value.length >=1 ){
+          if (roud1[0].checked){
+            window.alert('receita')
+          }else if (roud1[1].checked) {
+            window.alert('despesas')
+          }
+        }else{
+          window.alert('Sem Valor')
+        }
   }
